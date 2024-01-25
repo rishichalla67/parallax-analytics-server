5d0d46d3f87aa9af641cfb69ca610cc164bc5566
rchalla67 <rishichalla67@gmail.com>
2024-01-25 18:20:18 -0500

    added cache

--- src/api/ophir.js
+++ src/api/ophir.js
@@ -189,17 +189,17 @@ async function caclulateAndAddTotalTreasuryValue(balances) {
     if (!cache.coinGeckoPrices) {
         statData = await fetchStatData();
     }
-    const whalePrice = statData.coinGeckoPrices.data['white-whale']?.usd;
-    const whiteWhalePoolFilteredData = filterPoolsWithPrice(statData.whiteWhalePoolRawData.data);
+    const whalePrice = statData?.coinGeckoPrices.data['white-whale']?.usd || cache?.coinGeckoPrices.data['white-whale']?.usd;
+    const whiteWhalePoolFilteredData = filterPoolsWithPrice(statData?.whiteWhalePoolRawData.data || cache.whiteWhalePoolRawData.data) || 0;
 
     prices = {
         whale: whalePrice,
         ophir: whiteWhalePoolFilteredData["OPHIR-WHALE"] * whalePrice,
         bWhale: whiteWhalePoolFilteredData["bWHALE-WHALE"] * whalePrice,
         ampWhale: whiteWhalePoolFilteredData['ampWHALE-WHALE'] * whalePrice,
-        wBTC: statData.coinGeckoPrices.data['bitcoin']?.usd,
+        wBTC: statData?.coinGeckoPrices.data['bitcoin']?.usd || cache?.coinGeckoPrices.data['bitcoin']?.usd,
         ampWHALEt: whiteWhalePoolFilteredData["bWHALE-WHALE"] * whalePrice,  //update when there is a ampWHALEt pool
-        luna: statData.coinGeckoPrices.data["terra-luna-2"]?.usd,
+        luna: statData?.coinGeckoPrices.data["terra-luna-2"]?.usd || cache?.coinGeckoPrices.data['terra-luna-2']?.usd,
         ash: whiteWhalePoolFilteredData['ASH-WHALE'] * whalePrice
     }
 
