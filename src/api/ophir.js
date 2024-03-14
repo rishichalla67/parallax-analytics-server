7b7e7697b04ce0afba247b950d23aa9639c868d5
rchalla67 <rishichalla67@gmail.com>
2024-03-14 18:55:56 -0400

    updated ledger txn signing ux

--- src/api/ophir.js
+++ src/api/ophir.js
@@ -68,7 +68,8 @@ const tokenMappings = {
     'factory/migaloo1cwk3hg5g0rz32u6us8my045ge7es0jnmtfpwt50rv6nagk5aalasa733pt/ampUSDC': {symbol: 'ampUSDC', decimals: 6},
     'ibc/BC5C0BAFD19A5E4133FDA0F3E04AE1FBEE75A4A226554B2CBB021089FF2E1F8A': {symbol: 'axlUSDC', decimals: 6},
     'ibc/40C29143BF4153B365089E40E437B7AA819672646C45BB0A5F1E10915A0B6708': {symbol: 'bLuna', decimals: 6},
-    'ibc/05238E98A143496C8AF2B6067BABC84503909ECE9E45FBCBAC2CBA5C889FD82A': {symbol: 'ampLuna', decimals: 6}
+    'ibc/05238E98A143496C8AF2B6067BABC84503909ECE9E45FBCBAC2CBA5C889FD82A': {symbol: 'ampLuna', decimals: 6},
+    'factory/kujira16rujrka8vk3c7l7raa37km8eqcxv9z583p3c6e288q879rwp23ksy6efce/bOPHIR01': {symbol: "bOPHIR01", decimals: 6}
   };
 
 if (admin.apps.length === 0) {
@@ -538,12 +539,12 @@ async function caclulateAndAddTotalTreasuryValue(balances) {
     return {
         "totalTreasuryValue": formatNumber(totalValue, 2),
         "treasuryValueWithoutOphir": formatNumber(totalValueWithoutOphir, 2),
-        "ophirRedemptionPrice": calculateOphirRedeptionPrice(totalValueWithoutOphir, ophirStakedSupply, ophirMigalooVaultAmount)
+        "ophirRedemptionPrice": calculateOphirRedeptionPrice(totalValueWithoutOphir, ophirStakedSupply)
     };
 }
 
-function calculateOphirRedeptionPrice(totalValueWithoutOphir, ophirStakedSupply, ophirMigalooVaultAmount){
-    const adjTrueCirculatingSupply = cache.ophirCirculatingSupply.data+ophirStakedSupply-UNSOLD_OPHIR_FUZION_BONDS-ophirMigalooVaultAmount;
+function calculateOphirRedeptionPrice(totalValueWithoutOphir, ophirStakedSupply){
+    const adjTrueCirculatingSupply = cache.ophirCirculatingSupply.data+ophirStakedSupply;
     const adjTreasuryValue = totalValueWithoutOphir + ((1000000000 - adjTrueCirculatingSupply)*0.0025)  // (1,000,000,000 - (circ supply + staked ophir)) * 0.0025  done for calculating value added to treasury of all ophir sold at 0.0025 price
     return (adjTreasuryValue/adjTrueCirculatingSupply);
 }
